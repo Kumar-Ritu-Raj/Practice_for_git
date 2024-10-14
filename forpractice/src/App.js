91b6e9739cb71283ca988cb681b26e66524fc63a
KumarRituRaj <kumarrituraj2000@gmail.com>
2024-10-15 00:53:16 +0530

    add apartment form

--- forpractice/src/App.js
+++ forpractice/src/App.js
@@ -1,26 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import AddApartmentForm from "./components/AddApartmentForm.tsx";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ApartmentDetails from "./components/ApartmentDetails.tsx";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Welcome to this world</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          I am Second Branch
-          This branch is merged into main
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<AddApartmentForm />} />
+        <Route path="/apartment-details" element={<ApartmentDetails />} />
+      </Routes>
+    </Router>
   );
 }
 
